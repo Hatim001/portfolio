44835eea9252265e5710df04cde43f59bd8e43f8
Hatim Patrawala <hatimpatrawala786@gmail.com>
2024-06-20 10:35:56 -0300

    fix: error fixes in experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,16 +4,11 @@ type Props = {};
 
 const Experience = (props: Props) => {
   return (
-    <div className="my-5" style={{ minHeight: "70vh" }}> 
+    <div className="my-5" style={{ minHeight: "70vh" }}>
       <h1 className="display-4 mt-5">Experience</h1>
       <h1 className="bold-text bg-text">Profile</h1>
       <div className="row d-flex">
         <div className="col-md-10">
-          <h4 className="dmb-4 mt-2 profile-text">
-            I'm a Software Developer from India. Bees have 100 percent
-            employment, but we do jobs like taking the crud out.
-          </h4>
-
           <div className="mt-3">
             <div className="py-4">
               <div className="desc">
